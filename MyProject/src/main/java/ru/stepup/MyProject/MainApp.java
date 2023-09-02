d6a69e1dfc8bc058a58678a134e80f86b213f88f
druzhkov anton <karatevlg@gmail.com>
2023-09-02 14:29:34 +0300

    Добавление еще одного Хело Ворлд 2

--- MyProject/src/main/java/ru/stepup/MyProject/MainApp.java
+++ MyProject/src/main/java/ru/stepup/MyProject/MainApp.java
@@ -3,5 +3,6 @@ package ru.stepup.MyProject;
 public class MainApp {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Hello World 2");
     }
 }
